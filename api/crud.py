dd07b76b92e8559bce7ada12b6db6f8a4e1c8b14
Santiago_Torres <felipetorres260@gmail.com>
2024-09-23 21:49:08 -0500

    Upload CRUD

---
+++ api/crud.py
@@ -0,0 +1,11 @@
+'''
+C => Create (INSERT INTO)
+R => Read(SELECT)
+U => Update (UPDATE)
+D => Delete (DELTE)
+
+UPDATE AND DELETE NEED A WHERE CLAUSE *
+'''
+
+def create_user():
+    new_user_query = "INSERT INTO users ()"
